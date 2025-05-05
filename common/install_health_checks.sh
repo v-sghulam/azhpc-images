d5ffdbed953a7391fac862ab139f8cb3ffe4c5b7
vsghulam <v-sghulam@microsoft.com>
2025-05-05 12:33:16 -0700

    update health checks

--- common/install_health_checks.sh
+++ common/install_health_checks.sh
@@ -6,26 +6,31 @@ source ${COMMON_DIR}/utilities.sh
 
 aznhc_metadata=$(get_component_config "aznhc")
 AZHC_VERSION=$(jq -r '.version' <<< $aznhc_metadata)
-AZHC_SHA=$(jq -r '.sha256' <<< $aznhc_metadata)
 
 DEST_TEST_DIR=/opt/azurehpc/test
 GPU_PLAT=$1
 
-TARBALL="v${AZHC_VERSION}.tar.gz"
-AZHC_DOWNLOAD_URL=https://github.com/Azure/azurehpc-health-checks/archive/refs/tags/${TARBALL}
-$COMMON_DIR/download_and_verify.sh ${AZHC_DOWNLOAD_URL} ${AZHC_SHA} $DEST_TEST_DIR
+mkdir -p $DEST_TEST_DIR
 
 pushd $DEST_TEST_DIR
-mkdir azurehpc-health-checks && tar -xvf $TARBALL --strip-components=1 -C azurehpc-health-checks  
-pushd azurehpc-health-checks
-chmod +x ./dockerfile/pull-image-mcr.sh
-# Pull down docker container from MCR
-if [ "${GPU_PLAT}" = "AMD" ]; then
-   ./dockerfile/pull-image-mcr.sh rocm
+
+if [ "${GPU_PLAT}" = "NVIDIA" ]; then
+   git clone https://github.com/Azure/azurehpc-health-checks.git --branch v$AZHC_VERSION
+
+   pushd azurehpc-health-checks
+
+   # Pull down docker container from MCR
+   ./dockerfile/pull-image-acr.sh cuda
+   popd
 else
-   ./dockerfile/pull-image-mcr.sh cuda
+   git clone https://github.com/Azure/azurehpc-health-checks.git
+   pushd azurehpc-health-checks
+   # Build docker image for AMD while waiting to be published on MCR
+   ./dockerfile/build_image.sh rocm
+
+   popd
 fi
-popd
+
 popd
 
 $COMMON_DIR/write_component_version.sh "AZ_HEALTH_CHECKS" ${AZHC_VERSION}
