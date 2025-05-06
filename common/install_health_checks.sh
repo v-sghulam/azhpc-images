cc8d7f45be5f1036ac02639dff0d673d44c28b1f
vsghulam <v-sghulam@microsoft.com>
2025-05-06 08:16:04 -0700

    update aznhc to 0.4.4

--- common/install_health_checks.sh
+++ common/install_health_checks.sh
@@ -8,22 +8,29 @@ aznhc_metadata=$(get_component_config "aznhc")
 AZHC_VERSION=$(jq -r '.version' <<< $aznhc_metadata)
 
 DEST_TEST_DIR=/opt/azurehpc/test
+GPU_PLAT=$1
 
 mkdir -p $DEST_TEST_DIR
 
 pushd $DEST_TEST_DIR
 
-git clone https://github.com/Azure/azurehpc-health-checks.git --branch v$AZHC_VERSION
+if [ "${GPU_PLAT}" = "NVIDIA" ]; then
+   git clone https://github.com/Azure/azurehpc-health-checks.git --branch v$AZHC_VERSION
 
-pushd azurehpc-health-checks
-chmod +x ./dockerfile/pull-image-mcr.sh
-# Pull down docker container from MCR
-if [ "${GPU_PLAT}" = "AMD" ]; then
-   ./dockerfile/pull-image-mcr.sh rocm
+   pushd azurehpc-health-checks
+
+   # Pull down docker container from MCR
+   ./dockerfile/pull-image-acr.sh cuda
+   popd
 else
-   ./dockerfile/pull-image-mcr.sh cuda
+   git clone https://github.com/Azure/azurehpc-health-checks.git
+   pushd azurehpc-health-checks
+   # Build docker image for AMD while waiting to be published on MCR
+   ./dockerfile/build_image.sh rocm
+
+   popd
 fi
-popd
+
 popd
 
 $COMMON_DIR/write_component_version.sh "AZ_HEALTH_CHECKS" ${AZHC_VERSION}
