ee8dd91ac1e60d105614fc2720e72254e7839e32
vsghulam <v-sghulam@microsoft.com>
2025-05-06 06:50:12 -0700

    update aznhc

--- common/install_health_checks.sh
+++ common/install_health_checks.sh
@@ -6,17 +6,15 @@ source ${COMMON_DIR}/utilities.sh
 
 aznhc_metadata=$(get_component_config "aznhc")
 AZHC_VERSION=$(jq -r '.version' <<< $aznhc_metadata)
-AZHC_SHA=$(jq -r '.sha256' <<< $aznhc_metadata)
 
 DEST_TEST_DIR=/opt/azurehpc/test
-GPU_PLAT=$1
 
-TARBALL="v${AZHC_VERSION}.tar.gz"
-AZHC_DOWNLOAD_URL=https://github.com/Azure/azurehpc-health-checks/archive/refs/tags/${TARBALL}
-$COMMON_DIR/download_and_verify.sh ${AZHC_DOWNLOAD_URL} ${AZHC_SHA} $DEST_TEST_DIR
+mkdir -p $DEST_TEST_DIR
 
 pushd $DEST_TEST_DIR
-mkdir azurehpc-health-checks && tar -xvf $TARBALL --strip-components=1 -C azurehpc-health-checks  
+
+git clone https://github.com/Azure/azurehpc-health-checks.git --branch v$AZHC_VERSION
+
 pushd azurehpc-health-checks
 chmod +x ./dockerfile/pull-image-mcr.sh
 # Pull down docker container from MCR
