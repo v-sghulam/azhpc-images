9708e76a3bb076af0d3add0f9576a61ab5128491
vsghulam <v-sghulam@microsoft.com>
2025-05-15 13:21:21 -0700

    replicating 19.2.2

--- azurelinux/common/install_rocm.sh
+++ azurelinux/common/install_rocm.sh
@@ -15,7 +15,7 @@ tdnf -y install kernel-drivers-gpu-$(uname -r)
 #  https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-firmware-6.10.5.60302_2109964-1.azl3.noarch.rpm  \
 #  https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-headers-6.10.5.60302_2109964-1.azl3.noarch.rpm
                
-tdnf -y install https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-6.10.5.60302_2109964-1_6.6.82.1.1.azl3.x86_64.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-firmware-6.8.5.60202_2078359-6.azl3.noarch.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-headers-6.8.5.60202_2078359-6.azl3.noarch.rpm
+tdnf -y install https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-6.10.5.60302_2109964-1_6.6.82.1.1.azl3.x86_64.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-firmware-6.10.5.60302_2109964-1.azl3.noarch.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-headers-6.10.5.60302_2109964-1.azl3.noarch.rpm
 
 rocm_metadata=$(get_component_config "ROCM")
 ROCM_VERSION=$(jq -r '.version' <<< $rocm_metadata)
