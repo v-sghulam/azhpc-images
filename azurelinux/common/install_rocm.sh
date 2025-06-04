fa4ca9dfb3d63159fab70a7c9965cef58a5cb830
vsghulam <v-sghulam@microsoft.com>
2025-06-04 17:43:16 -0400

    adding --nogpgcheck  into amd driver

--- azurelinux/common/install_rocm.sh
+++ azurelinux/common/install_rocm.sh
@@ -13,7 +13,7 @@ tdnf -y install kernel-drivers-gpu-$(uname -r)
 #                 amdgpu-headers
 # 
 #tdnf -y install https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-6.10.5.60302_2109964-1_6.6.82.1.1.azl3.x86_64.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-firmware-6.10.5.60302_2109964-1.azl3.noarch.rpm https://packages.microsoft.com/azurelinux/3.0/prod/amd/x86_64/Packages/a/amdgpu-headers-6.10.5.60302_2109964-1.azl3.noarch.rpm
-tdnf -y install https://repo.radeon.com/amdgpu-install/6.4/azurelinux/3.0/amdgpu-install-6.4.60400-1.azl3.noarch.rpm
+tdnf -y install https://repo.radeon.com/amdgpu-install/6.4/azurelinux/3.0/amdgpu-install-6.4.60400-1.azl3.noarch.rpm --nogpgcheck 
 rocm_metadata=$(get_component_config "ROCM")
 ROCM_VERSION=$(jq -r '.version' <<< $rocm_metadata)
 # Add Azure Linux 3 ROCM repo file
