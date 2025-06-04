72452013cdee12909efc8a8605c08ab803ab92ff
vsghulam <v-sghulam@microsoft.com>
2025-06-04 15:13:08 -0400

    updating mlnx to 24.10.6

--- azurelinux/common/install_mellanoxofed.sh
+++ azurelinux/common/install_mellanoxofed.sh
@@ -98,18 +98,18 @@ tdnf install -y libibumad \
                 librdmacm-utils \
                 rdma-core \
                 rdma-core-devel \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-24.10-11.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-modules-24.10-11.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-devel-24.10-11.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-source-24.10-11.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-24.10-6.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-modules-24.10-6.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-devel-24.10-6.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-source-24.10-6.azl3.x86_64.rpm \
                 https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mft_kernel-4.30.0-13.azl3.x86_64.rpm \
                 mstflint \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/f/fwctl-24.10-11.azl3.x86_64.rpm  \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/f/fwctl-24.10-6.azl3.x86_64.rpm  \
                 ibacm \
                 ibarr \
                 ibsim \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/iser-24.10-11.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/isert-24.10-11.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/iser-24.10-6.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/isert-24.10-6.azl3.x86_64.rpm \
                 https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/k/knem-1.1.4.90mlnx3-13.azl3.x86_64.rpm \
                 https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/k/knem-modules-1.1.4.90mlnx3-13.azl3.x86_64.rpm \
                 perftest \
@@ -117,9 +117,9 @@ tdnf install -y libibumad \
                 libxpmem-devel \
                 mlnx-ethtool \
                 mlnx-iproute2 \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-nfsrdma-24.10-11.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-nfsrdma-24.10-6.azl3.x86_64.rpm \
                 multiperf \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/s/srp-24.10-11.azl3.x86_64.rpm \
+                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/s/srp-24.10-6.azl3.x86_64.rpm \
                 srp_daemon \
                 ucx \
                 ucx-cma \
